408c3321777fdf7a8f6a02ab98ad89e38f660420
ranngoo <62993735+ranngoo@users.noreply.github.com>
2022-05-28 21:31:03 +0200

    Update group_chat.php
    xd

--- group_chat.php
+++ group_chat.php
@@ -9,7 +9,7 @@ session_start();
 if($_POST["action"] == "insert_data")
 {
 	$data = array(
-		':from_user_id'		=>	$_SESSION["user_id"],
+		':from_user_id'		=>	$_SESSION['user_id'],
 		':chat_message'		=>	$_POST['chat_message'],
 		':status'			=>	'1'
 	);
