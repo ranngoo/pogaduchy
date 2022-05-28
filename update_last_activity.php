9afae2cd5864b9a8848a6b8aa88b7832a77ebae9
ranngoo <62993735+ranngoo@users.noreply.github.com>
2022-05-28 22:59:03 +0200

    Update update_last_activity.php
    ehh

--- update_last_activity.php
+++ update_last_activity.php
@@ -6,14 +6,20 @@ include('database_connection.php');
 
 session_start();
 
+if(isset($_SESSION['login_details_id']))
+{
+ 
 $query = "
 UPDATE login_details 
 SET last_activity = now() 
-WHERE login_details_id = '".$_SESSION['login_details_id']."'
+WHERE login_details_id = '".$_POST['login_details_id']."'
 ";
 
 $statement = $connect->prepare($query);
 
 $statement->execute();
+}elseif{
+  console.log("DUPA");
+}
 
 ?>
