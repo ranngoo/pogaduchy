952526a4c39bef5a9e55bfd0b20710385d037a76
ranngoo <62993735+ranngoo@users.noreply.github.com>
2022-05-29 00:32:17 +0200

    Update update_last_activity.php

--- update_last_activity.php
+++ update_last_activity.php
@@ -4,19 +4,13 @@ include('database_connection.php');
 
 session_start();
 
-$_SESSION['user_id'] = $log_id;
 
-$que1 = "select login_details_id from login_details 
-where user_id =".$log_id;
- $st = $connect->prepare($que1);
- 
 $query = "
 UPDATE login_details 
 SET last_activity = now() 
-WHERE login_details_id = ".$st;
+WHERE login_details_id = ".$_SESSION['login_details_id'];
 
 $statement = $connect->prepare($query);
-$st->execute();
 $statement->execute();
 
 ?>
