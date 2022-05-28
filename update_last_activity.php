b79d8de55a62c77dd26144b1e43582767e5380f2
ranngoo <62993735+ranngoo@users.noreply.github.com>
2022-05-28 23:26:05 +0200

    Update update_last_activity.php
    zmienna

--- update_last_activity.php
+++ update_last_activity.php
@@ -6,13 +6,15 @@ include('database_connection.php');
 
 session_start();
 
-if(isset($_SESSION['login_details_id']))
+$_SESSION['login_details_id'] = $log_id;
+
+if(isset($log_id))
 {
  
 $query = "
 UPDATE login_details 
 SET last_activity = now() 
-WHERE login_details_id = ".$_SESSION['login_details_id'];
+WHERE login_details_id = ".$log_id;
 
 $statement = $connect->prepare($query);
 
