ecb2faf3a2ae52daf252afcfe72f7f59d999158d
MiniTrupacc <62775771+MiniTrupacc@users.noreply.github.com>
2022-05-29 16:25:40 +0200

    Update group_chat.php

--- group_chat.php
+++ group_chat.php
@@ -7,8 +7,7 @@ if($_POST["action"] == "insert_data")
 {
 	$data = array(
 		':from_user_id'		=>	$_SESSION['user_id'],
-		':chat_message'		=>	$_POST['chat_message'],
-		':status'			=>	'1',
+		':chat_message'		=>	$_POST['chat_message']
 	);
 
 	$query = "
